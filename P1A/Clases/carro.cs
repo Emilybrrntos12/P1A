483240f870eb376ee76aa2e519a5139744ee4003
Emily Barrientos <Emily Barrientos@LAPTOP-MLHRD9JT>
2022-03-27 19:45:56 -0600

    Agregar archivos de proyecto.

---
+++ P1A/Clases/carro.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1A.Clases
+{
+    internal class carro
+    {
+        public string Marca { get;  }
+        public int Modelo { get;  }
+        public String Color { get; set; }
+
+        public int Maxvel { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+        private const int MAXVELOCIDAD = 150;
+
+        //public int MAXVELOCIDAD { get; }
+
+        public carro(string _marca, int _modelo, string _color, int _maxvel )
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Color = _color;
+            Maxvel = _maxvel;
+            Encendido = 0;
+            //MAXVELOCIDAD = _maxvelocidad;
+            this.velocidad_actual = 0;
+            
+        }
+
+        ////contruir 2do constructor
+        //public carro(string _marca, int _modelo, int _maxvelocidad )
+        //{
+        //    Marca= _marca;
+        //    Modelo = _maxvelocidad;
+        //    MAXVELOCIDAD = _maxvelocidad;
+        //    this.velocidad_actual= 0;
+        //}
+        public string GetVelocidadActual()
+        {
+            return $"\nVamos a {velocidad_actual} KPH";
+        }
+
+        public void EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+                Console.WriteLine("\nRUN RUN RUN RUN, LISTO PARA IRNOS\n");
+                Encendido = 1;
+                this.velocidad_actual = 0;
+            } else
+            {
+                Console.WriteLine("Orale no has encendido el motor");
+            }
+        }
+
+        public string acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el ccarro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 10;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            } else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH Aguas vas muy rapido y ya llegaste al tope";
+            }
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        public string acelerar(int CuantosKPH)
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"No puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += CuantosKPH;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            }else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH Aguas rapido y furioso, vas muy rapido ya lleagste al tope";
+            }
+
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        public string desacelerar( int Disminucion)
+        {
+            string mensaje = "";
+            velocidad_actual -= Disminucion;
+            if (velocidad_actual >= 0)
+            {
+                mensaje = $"--Empieza la desaleracion {velocidad_actual} KPH";
+            }
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        public string frenar()
+        {
+
+            string mesnaje = $"\tEl carro se ha detenido a {velocidad_actual} KPH";
+
+            Console.WriteLine(mesnaje);
+            return mesnaje;
+
+        }
+
+
+        public string apagar()
+        {
+            string mesnaje = "\n\tEl carro se a apagado";
+
+            Console.WriteLine(mesnaje);
+            return mesnaje;
+
+
+        }
+
+        public string bocinar()
+        {
+            string mesnaje = "------PIN PIN PIIIIIIIIIIIN-------";
+
+            Console.WriteLine(mesnaje);
+            return mesnaje;
+        }
+
+
+    }
+}
---
+++ P1A/Program.cs
@@ -0,0 +1,89 @@
+﻿using P1A.Clases;
+
+string linea, linea1, linea2, linea3;
+//carro miCarrito = new carro("Toyota", 2022, "azul");
+carro EmilyCarrito = new carro("BMW", 2022, "Azul", 350);
+
+
+//if (miCarrito.Modelo > EstefaniCarrito.Modelo)
+//{
+//    Console.WriteLine("El carro del profe es mas reciente");
+//}
+//else
+//{
+//    Console.WriteLine("El carro de Estefani es mas reciente");
+//}
+
+Console.WriteLine($"\tMi carrito es Marca {EmilyCarrito.Marca}");
+Console.WriteLine($"\tModelo {EmilyCarrito.Modelo}");
+Console.WriteLine($"\tDe Color {EmilyCarrito.Color}");
+Console.WriteLine($"\tY con una Maxima Velocidad de {EmilyCarrito.Maxvel}");
+
+//Encender motor
+Console.WriteLine(EmilyCarrito.GetVelocidadActual());
+Console.Write("Desea encender el carro? S/N: ");
+linea = Console.ReadLine();
+
+if (linea == "S")
+{
+    EmilyCarrito.EncenderMotor();
+}
+
+
+
+for (int i = 0; i < 5; i++)
+{
+    EmilyCarrito.acelerar();
+}
+
+EmilyCarrito.acelerar(3);
+EmilyCarrito.acelerar(6);
+EmilyCarrito.acelerar(1);
+
+//desaleración
+Console.WriteLine("\nCOMIENZA LA DESACELERACIÓN");
+EmilyCarrito.desacelerar(10);
+EmilyCarrito.desacelerar(5);
+EmilyCarrito.desacelerar(3);
+EmilyCarrito.desacelerar(1);
+
+//frenado
+Console.Write("\nDesea frenar el carro? S/N: ");
+linea = Console.ReadLine();
+
+if (linea == "S")
+{
+    EmilyCarrito.frenar();
+}
+else if (linea == "N")
+{
+    Console.WriteLine($"El carro sigue en marcha");
+}
+
+//apgar motor
+
+Console.Write("\nDesea apagar el carro? S/N: ");
+linea3 = Console.ReadLine();
+if (linea3 == "S")
+{
+    EmilyCarrito.apagar();
+}
+else if (linea3 == "N")
+{
+    Console.WriteLine("El carro sigue encendido");
+}
+
+//bocinar 
+Console.Write("\nDesea bocinar? S/N: ");
+linea2 = Console.ReadLine();
+
+if (linea2 == "S")
+{
+    EmilyCarrito.bocinar();
+}
+else if (linea2 == "N")
+{
+    Console.WriteLine("");
+}
+
+Console.WriteLine("\n\t\tGRACIAS POR UTILIZAR EL PROGRAMA!");
